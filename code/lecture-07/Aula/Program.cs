824d0500ea75104b2c845f5453ada14d00c651d6
mirko.viroli <mirko.viroli@unibo.it>
2021-11-29 22:17:16 +0100

    06 + 07 done

---
+++ code/lecture-07/Aula/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aula
+{
+   class UseGeneralizedHelpers
+   {
+      static void Main(string[] args)
+      {
+         var list = new List<string>(new[] {"a","bb","ccc","dddd"});
+         Helpers.ForEach(list,Console.WriteLine);
+         Console.WriteLine();
+
+         Helpers.ForEach(Helpers.Filter(list,s=>s.Length>1),Console.WriteLine);
+         
+         Helpers.ForEach(Helpers.Map(list,s=>s.Length),Console.WriteLine);
+
+         IEnumerable<string> ie = null;
+         // Console.WriteLine(ie);
+
+      }
+   }
+   class Helpers {
+      
+      public static void ForEach<T>(IEnumerable<T> elems, Action<T> action)
+      {
+         foreach (var e in elems) action(e); 
+      }
+      
+      public static IEnumerable<T> Filter<T>(IEnumerable<T> elems, Predicate<T> condition)
+      {
+         foreach (var e in elems) if (condition(e)) yield return e; 
+      }
+      
+      public static IEnumerable<TO> Map<TI,TO>(IEnumerable<TI> elems, Func<TI,TO> fun)
+      {
+         foreach (var e in elems) yield return fun(e); 
+      }
+      
+      public static int SumAll(IEnumerable<int> elems)
+      {
+         var sum = 0;
+         foreach (var e in elems) sum += e;
+         return sum;
+      }
+
+      public static IEnumerable<int> Iterate(int size)
+      {
+         for (var i=0; i<size; i++) yield return i;
+      }
+   }
+   
+   
+}
---
+++ code/lecture-07/BankAccountLambdas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using BankAccountDelegates;
+
+namespace BankAccountLambdas
+{
+   class UseFlexibleBankAccounts
+   {
+      static void Main(string[] args)
+      {
+         // use of anonymous function in-line where needed
+         var ba1 = new FlexibleBankAccount(
+            "a",
+            _ => 1, // lambda of the form x=><exp>
+            (_, a) => Console.WriteLine("Could not withdraw " + a)); 
+         ba1.Deposit(1000); // 1000
+         ba1.Withdraw(50); // 950
+         ba1.Withdraw(150); // 699
+         ba1.Withdraw(1000); // 699, + console output
+         Console.WriteLine(ba1.Balance); // 699
+
+         WithdrawAction act2 = (_, a) => Console.WriteLine("Could not withdraw " + a); 
+         act2 +=  (b, a) => Console.Error.WriteLine($"ERROR on Withdrawal: {b}:{a}"); 
+         var ba2 = new FlexibleBankAccount("a", amount => amount > 100 ? 1 : 0 , act2);
+         ba2.Deposit(1000); // 1000
+         ba2.Withdraw(50); // 949
+         ba2.Withdraw(150); // 798
+         ba2.Withdraw(1000); // 798, + console/error outputs
+         Console.WriteLine(ba2.Balance); // 700
+
+         // Use in libraries...
+         List<int> list = new List<int>(new int[]{11,33,22,49,5});
+         list.Sort((i,j) => i%10 - j%10);
+         foreach(var i in list) Console.WriteLine(i); // 5,10,20,30,..
+         list.Sort((i,j) => j-i);
+         foreach(var i in list) Console.WriteLine(i); // 40,30,20,..
+
+         var accounts = new List<IBankAccount>(new[] {ba1, ba2});
+         accounts.Sort((ba1,ba2)=>ba1.Balance-ba2.Balance);
+         foreach(var i in accounts) Console.WriteLine(i); // 40,30,20,..
+      }
+   }
+}
